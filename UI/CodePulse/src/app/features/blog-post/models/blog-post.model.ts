476ba4764a92e8278bf4e0ded86ce16b3474b8d5
SebastianK <sebast0382@gmail.com>
2025-05-30 11:06:42 +0200

    add blog post service method

---
+++ UI/CodePulse/src/app/features/blog-post/models/blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface BlogPost{
+    title: string
+    shortDescription: string
+    content: string
+    featuredImageURL: string
+    urlHandle: string
+    createdAt: Date
+    author: string
+    isVisible: boolean
+}
