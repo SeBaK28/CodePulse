90156f1a5be342f897aa94ed33b28ae318c4e39f
SebastianK <sebast0382@gmail.com>
2025-05-30 11:08:00 +0200

    add blog post functionality

---
+++ api/Controllers/BlogPostController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models.Domain;
+using api.Models.Dtos;
+using api.Repository;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/blogPost")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        private readonly IBlogPost _blogRepo;
+
+        public BlogPostController(IBlogPost blogRepo)
+        {
+            _blogRepo = blogRepo;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewBlogPost([FromBody] AddBlogPostDto post)
+        {
+            var blog = new BlogPost
+            {
+                Title = post.Title,
+                ShortDescription = post.ShortDescription,
+                Content = post.Content,
+                FeaturedImageURL = post.FeaturedImageURL,
+                URLHandle = post.URLHandle,
+                CreatedAt = post.CreatedAt,
+                Author = post.Author,
+                IsVisible = post.IsVisible
+            };
+
+            await _blogRepo.AddBlogPostAsync(blog);
+
+            var response = new BlogPostDto
+            {
+                Id = blog.Id,
+                Title = blog.Title,
+                ShortDescription = blog.ShortDescription,
+                Content = blog.Content,
+                FeaturedImageURL = blog.FeaturedImageURL,
+                URLHandle = blog.URLHandle,
+                CreatedAt = blog.CreatedAt,
+                Author = blog.Author,
+                IsVisible = blog.IsVisible
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ api/Repository/BlogPostRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Interfaces;
+using api.Models.Domain;
+
+namespace api.Repository
+{
+    public class BlogPostRepository : IBlogPost
+    {
+        public readonly ApplicationDbContext _context;
+
+        public BlogPostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<BlogPost> AddBlogPostAsync(BlogPost post)
+        {
+            await _context.BlogPosts.AddAsync(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+    }
+}
